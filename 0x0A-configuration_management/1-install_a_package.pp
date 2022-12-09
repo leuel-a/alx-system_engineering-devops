9189075c71e469b860f6246872f37dcddd120d38
nefelibatala1993 <leuela1993@gmail.com>
2022-12-10 00:44:42 +0300

    Update 0-create_a_file.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,7 @@
-# install puppet-lint using Puppet
-
+# Install puppet-lint
 package { 'puppet-lint':
-  ensure   => '2.1.0',
-  provider => 'gem',
+  ensure   => '2.5.0',
+  provider => 'gem'
 }
+Footer
+

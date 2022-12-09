458efb65d7bfc2eb12fa6f4db8bb48d8edbf974c
nefelibatala1993 <leuela1993@gmail.com>
2022-12-10 00:31:07 +0300

    Update 0-create_a_file.pp

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,9 @@
-file { '/tmp/school':
-  ensure => file,
-  content => 'I love puppet',
-  owner => 'www-data',
-  group => 'www-data',
-  mode => '0744'
+# create a file in /tmp with specific requirements
+
+file { 'school':
+  path    => '/tmp/school',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
 }

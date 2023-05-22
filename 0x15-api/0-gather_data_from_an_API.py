6a8992f95bda5f21a5b9fdd4823e7414b93103da
nefelibatala1993 <leuela1993@gmail.com>
2023-05-22 14:02:21 +0300

    update first task"

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -6,30 +6,30 @@ list progress.
 import requests
 from sys import argv
 
+if __name__ == '__main__':
+    # Get the user id
+    userId = argv[1]
 
-# Get the user id
-userId = argv[1]
+    # First get the employee information
+    payload = {"id": userId}
+    r = requests.get('https://jsonplaceholder.typicode.com/users/', params=payload)
 
-# First get the employee information
-payload = {"id": userId}
-r = requests.get('https://jsonplaceholder.typicode.com/users/', params=payload)
+    user = r.json()[0]
 
-user = r.json()[0]
+    # Now get the task information about the employee
+    payload = {"userId": argv}
+    r = requests.get('https://jsonplaceholder.typicode.com/todos/', params=payload)
 
-# Now get the task information about the employee
-payload = {"userId": argv}
-r = requests.get('https://jsonplaceholder.typicode.com/todos/', params=payload)
+    tasks = r.json()
 
-tasks = r.json()
+    # count the tasks that they completed
+    count = 0
+    for task in tasks:
+        if task.get('completed'):
+            count += 1
 
-# count the tasks that they completed
-count = 0
-for task in tasks:
-    if task.get('completed'):
-        count += 1
+    print(f"Employee {user.get('name')} is done with tasks({count}/{len(tasks)}):")
 
-print(f"Employee {user.get('name')} is done with tasks({count}/{len(tasks)}):")
-
-for task in tasks:
-    if task.get('completed'):
-        print(f"\t{task['title']}")
+    for task in tasks:
+        if task.get('completed'):
+            print(f"\t{task['title']}")

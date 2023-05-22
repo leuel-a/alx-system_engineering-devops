59e87ca09a9e0ad1f5acaff0ac4a8ee66de7dc99
nefelibatala1993 <leuela1993@gmail.com>
2023-05-22 14:18:29 +0300

    update first task

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -4,26 +4,26 @@ import requests
 import sys
 
 
-def get_data(userId: str) -> None:
-    """fetches the data from the 'https://intranet.alxswe.com/rltoken/7cr7aLYdaWAZWBKrBKS12A'
-    using the user id provided"""
-    payload = {"id": userId}
-    r = requests.get('https://jsonplaceholder.typicode.com/users/', params=payload)
-    user = r.json()[0]
+# def get_data(userId: str) -> None:
+#     """fetches the data from the 'https://intranet.alxswe.com/rltoken/7cr7aLYdaWAZWBKrBKS12A'
+#     using the user id provided"""
+#     payload = {"id": userId}
+#     r = requests.get('https://jsonplaceholder.typicode.com/users/', params=payload)
+#     user = r.json()[0]
 
-    payload = {"userId": userId}
-    r = requests.get('https://jsonplaceholder.typicode.com/todos/', params=payload)
-    tasks = r.json()
+#     payload = {"userId": userId}
+#     r = requests.get('https://jsonplaceholder.typicode.com/todos/', params=payload)
+#     tasks = r.json()
 
-    count = 0
-    for task in tasks:
-        if task.get('completed'):
-            count += 1
-            
-    print(f"Employee {user.get('name')} is done with tasks({count}/{len(tasks)}):")
-    for task in tasks:
-        if task.get('completed'):
-            print(f"\t{task['title']}")
+#     count = 0
+#     for task in tasks:
+#         if task.get('completed'):
+#             count += 1
+
+#     print(f"Employee {user.get('name')} is done with tasks({count}/{len(tasks)}):")
+#     for task in tasks:
+#         if task.get('completed'):
+#             print(f"\t{task['title']}")
 
 
 if __name__ == '__main__':

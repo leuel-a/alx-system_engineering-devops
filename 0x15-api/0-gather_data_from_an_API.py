9a7a46ff81b9aa53dad689f7a0de6dbc822ecdb4
nefelibatala1993 <leuela1993@gmail.com>
2023-05-22 14:12:51 +0300

    update 0-gather_data_from_an_API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,2 +1,25 @@
 #!/usr/bin/python3
 """Fetches data from a REST API"""
+import requests
+import sys
+
+if __name__ == '__main__':
+    userId = sys.argv[1]
+
+    payload = {"id": userId}
+    r = requests.get('https://jsonplaceholder.typicode.com/users/', params=payload)
+    user = r.json()[0]
+
+    payload = {"userId": userId}
+    r = requests.get('https://jsonplaceholder.typicode.com/todos/', params=payload)
+    tasks = r.json()
+
+    count = 0
+    for task in tasks:
+        if task.get('completed'):
+            count += 1
+
+    print(f"Employee {user.get('name')} is done with tasks({count}/{len(tasks)}):")
+    for task in tasks:
+        if task.get('completed'):
+            print(f"\t{task['title']}")

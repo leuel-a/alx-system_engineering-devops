85e1b8589a86df89207f2d297afe488257d350bc
nefelibatala1993 <leuela1993@gmail.com>
2023-05-22 22:35:44 +0300

    finisded last task

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""fetches data from a REST API and exports it to a JSON file"""
+import json
+import sys
+import requests
+
+
+def export_all() -> None:
+    """exports all the user information to a json file"""
+    r = requests.get('https://jsonplaceholder.typicode.com/users/')
+    users = r.json()
+
+    r = requests.get('https://jsonplaceholder.typicode.com/todos/')
+    tasks = r.json()
+
+    all = {}
+    for user in users:
+        id = user.get('id')
+        uname = user.get('username')
+
+        lst = []
+        for task in tasks:
+            if task.get('userId') == id:
+                title = task.get('title')
+                completed = task.get('completed')
+                lst.append({"username": uname, "task": title, "completed": completed})
+        all[id] = lst
+
+    with open("todo_all_employees.json", "w") as fp:
+        json.dump(all, fp)
+
+
+if __name__ == '__main__':
+    export_all()

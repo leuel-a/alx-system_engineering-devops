6182dd5fdcee8f4c7a6628850ed02119e39292bf
nefelibatala1993 <leuela1993@gmail.com>
2023-05-22 14:16:28 +0300

    update first task

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
-if __name__ == '__main__':
-    """Fetches data from a REST API"""
-    import requests
-    import sys
-    userId = sys.argv[1]
+"""Fetches data from a REST API"""
+import requests
+import sys
 
+
+def get_data(userId: str) -> None:
+    """fetches the data from the 'https://intranet.alxswe.com/rltoken/7cr7aLYdaWAZWBKrBKS12A'
+    using the user id provided"""
     payload = {"id": userId}
     r = requests.get('https://jsonplaceholder.typicode.com/users/', params=payload)
     user = r.json()[0]
@@ -17,8 +19,12 @@ if __name__ == '__main__':
     for task in tasks:
         if task.get('completed'):
             count += 1
-
+            
     print(f"Employee {user.get('name')} is done with tasks({count}/{len(tasks)}):")
     for task in tasks:
         if task.get('completed'):
             print(f"\t{task['title']}")
+
+
+if __name__ == '__main__':
+    userId = sys.argv[1]
